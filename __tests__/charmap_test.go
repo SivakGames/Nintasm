0c4bc4e36e5f7cd9378040a48e94ddddbc0339f4
Sivak <sivakgames@gmail.com>
2024-02-20 08:59:13 +0900

    Test double byte chars

--- __tests__/charmap_test.go
+++ __tests__/charmap_test.go
@@ -18,29 +18,29 @@ func TestCharmaps(t *testing.T) {
 		moduleLines = append(moduleLines, " .defchar \"C\", $12")
 		moduleLines = append(moduleLines, " .defchar \"Ä\", $13")
 		moduleLines = append(moduleLines, " .defchar \"Ô\", $14")
-		moduleLines = append(moduleLines, " .defchar \"Ý\", $15")
-		moduleLines = append(moduleLines, " .defchar \"┏\", $16")
-		moduleLines = append(moduleLines, " .defchar \"┓\", $17")
-		moduleLines = append(moduleLines, " .defchar \"┗\", $18")
-		moduleLines = append(moduleLines, " .defchar \"┛\", $19")
-		moduleLines = append(moduleLines, " .defchar \"あ\", $1a")
-		moduleLines = append(moduleLines, " .defchar \"い\", $1b")
-		moduleLines = append(moduleLines, " .defchar \"ガ\", $1c")
-		moduleLines = append(moduleLines, " .defchar \"ギ\", $1d")
-		moduleLines = append(moduleLines, " .defchar \"邪\", $1e")
-		moduleLines = append(moduleLines, " .defchar \"鬼\", $1f")
-		moduleLines = append(moduleLines, " .defchar \"王\", $20")
-		moduleLines = append(moduleLines, " .defchar \"💚\", $21")
-		moduleLines = append(moduleLines, " .defchar \"🧊\", $22")
+		moduleLines = append(moduleLines, " .defchar \"┏\", $15")
+		moduleLines = append(moduleLines, " .defchar \"┓\", $16")
+		moduleLines = append(moduleLines, " .defchar \"┗\", $17")
+		moduleLines = append(moduleLines, " .defchar \"┛\", $18")
+		moduleLines = append(moduleLines, " .defchar \"あ\", $19")
+		moduleLines = append(moduleLines, " .defchar \"い\", $1a")
+		moduleLines = append(moduleLines, " .defchar \"ガ\", $1b")
+		moduleLines = append(moduleLines, " .defchar \"ギ\", $1c")
+		moduleLines = append(moduleLines, " .defchar \"邪\", $1d")
+		moduleLines = append(moduleLines, " .defchar \"鬼\", $1e")
+		moduleLines = append(moduleLines, " .defchar \"王\", $1f")
+		moduleLines = append(moduleLines, " .defchar \"💚\", $20")
+		moduleLines = append(moduleLines, " .defchar \"🧊\", $21")
+		moduleLines = append(moduleLines, " .defchar \"➡\", $22,$23")
 		moduleLines = append(moduleLines, "testCharmap .endcharmap")
-		moduleLines = append(moduleLines, " .db toCharmap(\"ABCÄÔÝ┏┓┗┛あいガギ邪鬼王💚🧊\")")
+		moduleLines = append(moduleLines, " .db toCharmap(\"ABCÄÔ┏┓┗┛あいガギ邪鬼王💚🧊➡\")")
 
 		wanted := []uint8{
 			uint8(0x10), uint8(0x11), uint8(0x12), uint8(0x13),
 			uint8(0x14), uint8(0x15), uint8(0x16), uint8(0x17),
 			uint8(0x18), uint8(0x19), uint8(0x1a), uint8(0x1b),
 			uint8(0x1c), uint8(0x1d), uint8(0x1e), uint8(0x1f),
-			uint8(0x20), uint8(0x21), uint8(0x22),
+			uint8(0x20), uint8(0x21), uint8(0x22), uint8(0x23),
 		}
 
 		err := assemble.Start(moduleLines)
