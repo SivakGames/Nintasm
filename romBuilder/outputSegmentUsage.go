fd2591eff4fa138300f2af411bce1998edc62482
Sivak <sivakgames@gmail.com>
2024-03-24 10:58:40 +0900

    More with segment use visuals

--- romBuilder/outputSegmentUsage.go
+++ romBuilder/outputSegmentUsage.go
@@ -42,10 +42,10 @@ func OutputSegmentUsage() {
 			fmt.Println(bankOutput)
 		}
 		output := calcAndGenOutput(totalSegmentMax, totalSegmentOccupied)
-		segmentCaption := util.Colorize(" Total ", enumTerminalColors.AnsiDarkSeaGreen, true)
-		bankOutput := fmt.Sprintf("%v %v", segmentCaption, output)
+		segmentTotalCaption := util.Colorize(" Total ", enumTerminalColors.AnsiDarkSeaGreen, true)
+		segmentTotalOutput := fmt.Sprintf("%v%v %v", strings.Repeat(" ", bankCaptionNumberWidth), segmentTotalCaption, output)
 		fmt.Println(strings.Repeat("╌", 64))
-		fmt.Println(bankOutput)
+		fmt.Println(segmentTotalOutput)
 		fmt.Println()
 	}
 }
@@ -54,58 +54,17 @@ func calcAndGenOutput(maxBytes int, occupiedBytes int) string {
 	unoccupiedBytes := maxBytes - occupiedBytes
 	availablePercent := (float64(unoccupiedBytes) / float64(maxBytes)) * 100
 
-	out2 := commaSeparated(maxBytes)
-	out1 := util.PadStringLeft(commaSeparated(occupiedBytes), len(out2), ' ')
-	out3 := commaSeparated(unoccupiedBytes)
+	out2 := util.CommaSeparatedNumber(maxBytes)
+	out1 := util.PadStringLeft(util.CommaSeparatedNumber(occupiedBytes), len(out2), ' ')
+	//out3 := commaSeparated(unoccupiedBytes)
 	percentVisual := util.PadStringRight(fmt.Sprintf("(%.2f%%)", availablePercent), 9, ' ')
 	percentVisual = strings.ReplaceAll(percentVisual, "(", util.Colorize("(", enumTerminalColors.Cyan, false))
 	percentVisual = strings.ReplaceAll(percentVisual, ")", util.Colorize(")", enumTerminalColors.Cyan, false))
 
-	numBars := 0
+	barVisual := util.GeneratePercentBar(availablePercent)
 
-	for i := 0; i < 8; i++ {
-		threshold := float64(i) * 12.5
-		if availablePercent > threshold {
-			numBars++
-		}
-	}
-
-	g := int(5 * (float64(numBars) / 8.0))
-	r := int(5 * (float64(8-numBars) / 8.0))
-
-	solidBarVisual := strings.Repeat("█", numBars)
-	emptyBarVisual := strings.Repeat("░", 8-numBars)
-	barVisual := util.ColorizeWithAnsiRGBCode(solidBarVisual, fmt.Sprintf("%d%d0", r, g), false) + util.Colorize(emptyBarVisual, enumTerminalColors.AnsiGray4, false)
-
-	output := fmt.Sprintf("%v %v %v %v │ %v %v remaining",
+	output := fmt.Sprintf("%v %v %v │ %v %v",
 		out1, util.Colorize("/", enumTerminalColors.Cyan, false), out2,
-		percentVisual, barVisual, out3)
+		barVisual, percentVisual)
 	return output
-}
-
-func commaSeparated(value int) string {
-	var digits []string
-
-	if value == 0 {
-		digits = append(digits, "0")
-	}
-
-	for value > 0 {
-		insertValue := value % 1000
-		value /= 1000
-		if value > 0 {
-			digits = append(digits, fmt.Sprintf(",%03d", insertValue))
-		} else {
-			digits = append(digits, fmt.Sprintf("%d", insertValue))
-		}
-	}
-
-	finalString := ""
-	for len(digits) > 0 {
-		num := digits[len(digits)-1]
-		finalString += num
-		digits = digits[:len(digits)-1]
-	}
-
-	return finalString
 }
